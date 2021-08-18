6712cc631934afea79bc36d1563361f8119d6ff5
isaacherbst613 <isaacherbst613@gmail.com>
2021-08-17 19:59:12 -0700

    hw 63, this/call/apply

---
+++ homework/JS/63/bank.js
@@ -0,0 +1,36 @@
+(function(){
+    'use strict';
+
+    function account(){
+            return{ 
+            balance: 0,
+            performTransaction: function (amnt){
+                this.balance += amnt;
+                console.log('your new balance is $' + this.balance);
+            }
+        };
+    }
+
+    let acnt = account();
+    acnt.performTransaction(1000);
+    acnt.performTransaction(-500);
+
+    
+    
+    function transaction(amnt){
+            this.balance += amnt;
+            console.log('your new balance is $' + this.balance);
+    }
+
+    function account2(){
+        return{
+            balance: 0,
+        };
+    }
+
+    let acnt2 = account2();
+    transaction.call(acnt2, 5000);
+    transaction.call(acnt2, -1000);
+    transaction.apply(acnt2, [1000,-500,750]);
+
+}());
