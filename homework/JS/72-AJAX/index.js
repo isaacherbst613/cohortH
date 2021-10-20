4e15fbbe1e38f322c3d0ab1369d48328c268bbd1
isaacherbst613 <isaacherbst613@gmail.com>
2021-10-19 21:53:21 -0400

    added v.2 in index.js

--- homework/JS/72-AJAX/index.js
+++ homework/JS/72-AJAX/index.js
@@ -19,7 +19,6 @@
             const recipes = await response.json();
             Object.keys(recipes[0]).forEach(key => {
                 recipeList.append(`<option>${recipes[0][key]}</option>`)
-                console.log(recipes[0][key])
             });
 
          } catch (e) {
@@ -58,5 +57,44 @@
     }
 
 
+
+//v.2 less copied code, but i have a feeling extra awaits will hamper performance.....
+    // async function populate(url) {
+    //     const recipes = await fetcher(url);
+    //     Object.keys(recipes[0]).forEach(key => {
+    //         recipeList.append(`<option>${recipes[0][key]}</option>`)
+    //     });
+    // }
+
+    // async function display(url) {   
+    //     const recipe = await fetcher(url);
+    //     recipeList.change(() => {
+    //         const selRec = recipeList.find(":selected").text();//jQuery find() recipe chosen
+    //         const found = recipe.find((e) => selRec === e.name)//js find() match to fetched list
+
+    //         displayName.text(found.name);
+    //         pic.attr('src', found.img);
+    //         cardBtm.show();
+    //         ingreds.empty();//empty prev recipe ings from list
+
+    //         Object.keys(found.ingredients).forEach(ing => {
+    //             ingreds.append(`<li>${found.ingredients[ing]}</li>`)
+    //         });
+
+    //         dirs.text(found.directions);
+    //     })
+    // }
+
+    // async function fetcher(url) {
+    //     try {
+    //         const response = await fetch(url);
+    //         if (!response.ok) {
+    //             throw new Error(`${e.status} ${e.statusText}`);
+    //         }
+    //         return await response.json();
+    //     } catch (e) {
+    //         console.error(e)
+    //     }
+    // }
 
 }())
