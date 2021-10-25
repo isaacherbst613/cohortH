9d818b3f0a22b7fff6c3f7a08a37700bb6cd8764
isaacherbst613 <isaacherbst613@gmail.com>
2021-10-24 20:04:38 -0400

    hw 73 ajax vids

---
+++ homework/JS/73/index.js
@@ -0,0 +1,31 @@
+(function () {
+    'use strict';
+
+    const vidList = $("#vidList");
+    populate('vidsList.json')
+
+    async function populate(url) {
+        const videos = await fetcher(url);
+        videos.forEach(v => {
+            $(`<tr><td>${v.name}</td><td>${v.artist}</td></tr>`).appendTo(vidList)
+                .click(() => {
+                    $('#name').text(v.name);
+                    $('video').attr({'poster': v.img,'src': v.url});
+                    $('#bottom').show();
+                    $('#art').text(v.artist);
+                })
+        });
+    };
+
+    async function fetcher(url) {
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`${e.status} ${e.statusText}`);
+            }
+            return await response.json();
+        } catch (e) {
+            console.error(e)
+        }
+    }
+}())
